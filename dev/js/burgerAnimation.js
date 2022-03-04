bf8b798dc30b3de4d3d5a3339420a47989f43d74
sdb329 <sdb329@drexel.edu>
2022-03-04 18:48:04 -0500

    working on nav

--- dev/js/burgerAnimation.js
+++ dev/js/burgerAnimation.js
@@ -5,6 +5,12 @@ gsap.registerPlugin(DrawSVGPlugin);
 gsap.set("#burger",{transformOrigin:"center"});
 // gsap.set("#top",{drawSVG: "0% 37%"});
 gsap.set("#bottom",{drawSVG: "0% 48%"});
+// gsap.set("#circle-svg",{scale: .2, transformOrigin:"right center", y: "-=400", x: ""});
+gsap.set("#circle-svg",{scale: 0, y: "-=30", x: "+=30"});
+    
+
+
+
 
 
 // gsap.set("#burger",{rotate:90});
@@ -12,16 +18,20 @@ gsap.set("#bottom",{drawSVG: "0% 48%"});
 
 
 
-
+export const circleTL = new gsap.timeline({paused:true});
+circleTL.to("#circle-svg", {duration:1, scale: 50});
 
 const bottomTL = new gsap.timeline();
-bottomTL.to("#bottom",{duration:0.25, drawSVG: "80% 100%"})
+bottomTL.to("#bottom",{duration:0.25, drawSVG: "47% 100%"})
 .to("#bottom",{duration:0.25});
 
 export const burgerTL = new gsap.timeline({paused:true});
 
 burgerTL
-    .add(bottomTL,"burger");
+   
+    .add(bottomTL,"burger")
+    ;
+
 
 
 
